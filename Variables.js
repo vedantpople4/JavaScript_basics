1520c93db5669e36ba8761129dc86ab30d1d35c6
Vedant pople <vedantpople4@gmail.com>
2022-07-01 20:32:09 +0530

    Basics

--- Variables.js
+++ Variables.js
@@ -4,7 +4,11 @@ example = "Alice"
 
 let firstName = "Dylan";
 let lastName = "Rice";
-console.log((firstName + " " + lastName).split("")); 
+//console.log((firstName + " " + lastName).split("")); 
 
-
-
+let num = 7.87;
+console.log(parseFloat(num));
+console.log(num.toFixed(2));
+console.log(parseInt("33.43"))
+console.log(parseFloat("60"))
+console.log(200.0.toFixed(3))
