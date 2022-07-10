3bcfb761eaa8e9fff13c7c056daff41f39f7697d
Vedant pople <vedantpople4@gmail.com>
2022-07-10 22:44:19 +0530

    Variable.js

--- Variables.js
+++ Variables.js
@@ -12,3 +12,28 @@ console.log(num.toFixed(2));
 console.log(parseInt("33.43"))
 console.log(parseFloat("60"))
 console.log(200.0.toFixed(3))
+
+var a;
+console.log(a);
+var b = 2;
+var c = "I am a String";
+a = 7;
+b = a;
+var A = 4;
+console.log(a);
+console.log(A);
+var newVariable = "This is camel case";
+console.log(newVariable);
+
+var add = 3+5;
+var subtract = 34-31;
+var mul = 4*3;
+var divide = 21/3;
+console.log(add,subtract,mul,divide);
+add++;
+mul--;
+console.log(add, mul);
+
+
+
+
